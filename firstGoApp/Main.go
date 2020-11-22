0288f5126986ca667f3050cb658c73926b0ce70d
ivantxo <ivan.ramirezf@gmail.com>
2020-11-23 06:23:01 +1000

    More type convertion

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -11,4 +12,9 @@ func main() {
 	var j float32
 	j = float32(i)
 	fmt.Printf("%v, %T\n", j, j)
+
+	var k string
+	// k = string(i) // wrong type convertion to string
+	k = strconv.Itoa(i) // right way of type converting
+	fmt.Printf("%v, %T\n", k, k)
 }
