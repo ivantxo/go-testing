b891b310c5791b58881bf4c81497fff6ee4586b4
ivantxo <ivan.ramirezf@gmail.com>
2020-11-23 06:02:11 +1000

    Added variable declaration

---
+++ firstGoApp/Main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var i int = 42
+
+func main() {
+	// var i int
+	// i = 42
+
+	// var j int = 27
+
+	// k := 99.
+
+	// fmt.Println(i)
+	// fmt.Printf("%v, %T", k, k)
+	fmt.Printf("%v, %T", i, i)
+}
