6524e0e3f11d84eee1984810466cea5f02867c47
ivantxo <ivan.ramirezf@gmail.com>
2020-11-26 06:26:23 +1000

    Variable Types
    - Strings

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -5,10 +5,16 @@ import (
 )
 
 func main() {
-	a := 10             // 1010
-	b := 3              // 0011
-	fmt.Println(a & b)  // 0010
-	fmt.Println(a | b)  // 1011
-	fmt.Println(a ^ b)  // 1001
-	fmt.Println(a &^ b) // 0100
+	s := "this is a string"
+	fmt.Printf("%v, %T\n", s, s)
+	fmt.Printf("%v, %T\n", s[2], s[2])
+	// s[2] = "u" // this is wrong
+
+	// string concatenation
+	s2 := "this is also a string"
+	fmt.Printf("%v, %T\n", s+s2, s+s2)
+
+	// collection of bytes
+	b := []byte(s)
+	fmt.Printf("%v, %T\n", b, b)
 }
