3eee9bb2393961ab7000f79355dc972e74ee89d5
ivantxo <ivan.ramirezf@gmail.com>
2020-11-26 06:32:49 +1000

    Variable Types
    - Runes

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -5,16 +5,9 @@ import (
 )
 
 func main() {
-	s := "this is a string"
-	fmt.Printf("%v, %T\n", s, s)
-	fmt.Printf("%v, %T\n", s[2], s[2])
-	// s[2] = "u" // this is wrong
+	r := 'a'
+	fmt.Printf("%v, %T\n", r, r)
 
-	// string concatenation
-	s2 := "this is also a string"
-	fmt.Printf("%v, %T\n", s+s2, s+s2)
-
-	// collection of bytes
-	b := []byte(s)
-	fmt.Printf("%v, %T\n", b, b)
+	var ru rune = 'a'
+	fmt.Printf("%v, %T\n", ru, ru)
 }
