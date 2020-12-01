8d5a832c25f4c0e74d4f54ee10e19ee480e410b8
ivantxo <ivan.ramirezf@gmail.com>
2020-12-02 07:11:27 +1000

    Slices 1

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -5,38 +5,23 @@ import (
 )
 
 func main() {
-	grades := [3]int{95, 97, 98}    // array of size 3
-	grades2 := [...]int{95, 97, 98} // array which size is just the literal size {95, 97, 98}
-	fmt.Printf("Grades: %v\n", grades)
-	fmt.Printf("Grades: %v\n", grades2)
-
-	var students [3]string
-	students[0] = "Sonia"
-	students[1] = "Ivan"
-	students[2] = "Judith"
-	fmt.Printf("Grades: %v\n", students)
-	fmt.Printf("Student #1: %v\n", students[0])
-	fmt.Printf("Number of Students: %v\n", len(students))
-
-	// array of arrays
-	var identityMatrix [3][3]int
-	identityMatrix[0] = [3]int{1, 0, 0}
-	identityMatrix[0] = [3]int{1, 0, 0}
-	identityMatrix[0] = [3]int{1, 0, 0}
-	fmt.Printf("Identy Matrix: %v\n", identityMatrix)
-
-	// arrays don't share the same memory space
-	a := [...]int{1, 2, 3}
+	// slices share the same memory space
+	a := []int{1, 2, 3}
 	b := a
-	b[1] = 5
+	b[1] = 7
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println("")
 
-	// unless using pointers
-	c := &a
-	c[1] = 7
-	fmt.Println(a)
-	fmt.Println(b)
+	// more on slices
+	c := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	d := c[:]   // slice of all elements
+	e := c[3:]  // slice from 4th element to end
+	f := c[:6]  // slice of first 6 elements
+	g := c[3:6] // slice the 4th, 5th and 6th elements
 	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
 }
