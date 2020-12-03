91e2d4c1e66b278fea49ebe48a8ccb03357e9819
ivantxo <ivan.ramirezf@gmail.com>
2020-12-04 06:18:44 +1000

    Structs

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -4,45 +4,27 @@ import (
 	"fmt"
 )
 
+// Doctor structure
+type Doctor struct {
+	number     int
+	actorName  string
+	episodes   []string
+	companions []string
+}
+
 func main() {
-	// maps
+	// structs
 
-	// built-in function
-	// statePopulations := make(map[string]int)
-
-	// literal sintax
-	statePopulations := map[string]int{
-		"QLD": 123,
-		"NSW": 456,
-		"SA":  789,
-		"TAS": 012,
-		"VIC": 345,
-		"WA":  678,
+	aDoctor := Doctor{
+		number:    3,
+		actorName: "Jon Pertwee",
+		companions: []string{
+			"Liz Shaw",
+			"Jo Grant",
+			"Sarah Jane Smith",
+		},
 	}
-	fmt.Println(statePopulations)
-	fmt.Println(statePopulations["QLD"])
-	fmt.Println(statePopulations)
+	fmt.Println(aDoctor)
 
-	// Add new elements to map
-	statePopulations["Papua"] = 455
-	fmt.Println(statePopulations["Papua"])
-	// The order of elements in a map is not guaranted to be the same
-	fmt.Println(statePopulations)
-
-	// delete elements from a map
-	delete(statePopulations, "Papua")
-	fmt.Println(statePopulations)
-
-	// get an element with the comma, ok syntax
-	pop, ok := statePopulations["Papua"]
-	fmt.Println(pop, ok)
-
-	// size of a map
-	fmt.Println(len(statePopulations))
-
-	// data is passed by reference
-	sp := statePopulations
-	delete(sp, "QLD")
-	fmt.Println(sp)
-	fmt.Println(statePopulations)
+	fmt.Println(aDoctor.companions)
 }
