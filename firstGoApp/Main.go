313f9f7d9c24b7a1e29ea3226ee04b263c0b58b8
ivantxo <ivan.ramirezf@gmail.com>
2020-12-05 05:43:49 +1000

    If controls. Short circuiting

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -1,19 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
-// Animal struct
-type Animal struct {
-	Name   string `required max: "100"`
-	Origin string
-}
+import "fmt"
 
 func main() {
-	// tags
-	t := reflect.TypeOf(Animal{})
-	field, _ := t.FieldByName("Name")
-	fmt.Println(field.Tag)
+	guess := -5
+	// short circuiting: in this case guess is less than 1, then it doesn't evaluate the
+	// other conditions, it just assumes is true.
+	if guess < 1 || returnTrue() || guess > 100 {
+		fmt.Println("The guest must be between 1 and 100")
+	}
+}
+
+func returnTrue() bool {
+	fmt.Println("Returning true")
+	return true
 }
