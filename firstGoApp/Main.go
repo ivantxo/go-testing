9b9ba5960225685f2c7a59c74cfb5ef42aa29c71
ivantxo <ivan.ramirezf@gmail.com>
2020-12-05 06:03:59 +1000

    Switch statements, tagless switch

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	switch 4 {
-	case 1, 5, 10:
-		fmt.Println("one, five, or ten")
-	case 2, 4, 6:
-		fmt.Println("two, four or six")
+	i := 10
+	switch { // <== tagless switch statement
+	case i <= 10:
+		fmt.Println("less than or equal to ten")
+
+	case i <= 20:
+		fmt.Println("less than or equal to twenty")
 
 	default:
 		fmt.Println("another number")
