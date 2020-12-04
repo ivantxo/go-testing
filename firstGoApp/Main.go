a37db676bc4cc1d4cb0ce9f92a8976e349ed5d63
ivantxo <ivan.ramirezf@gmail.com>
2020-12-05 06:08:35 +1000

    Switch statements, fallthrough operator

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -7,6 +7,7 @@ func main() {
 	switch { // <== tagless switch statement
 	case i <= 10:
 		fmt.Println("less than or equal to ten")
+		fallthrough // it will fall through the next case
 
 	case i <= 20:
 		fmt.Println("less than or equal to twenty")
