b0d371184953a303f79a8d3588cd0a9577d89071
ivantxo <ivan.ramirezf@gmail.com>
2020-12-07 05:57:45 +1000

    for loops, iterating slices, arrays, maps and strings

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -3,14 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	// Adding tags is a good way to break an outer loop
-Loop:
-	for i := 1; i <= 3; i++ {
-		for j := 1; j <= 3; j++ {
-			fmt.Println(i * j)
-			if i*j >= 2 {
-				break Loop
-			}
-		}
+	// loop slices or arrays s := [3]string{"a", "b", "c"}
+	s := []string{"a", "b", "c"}
+	for k, v := range s {
+		fmt.Println(k, v)
+	}
+	fmt.Println()
+
+	// loop over maps
+	statePopulations := map[string]int{
+		"NSW": 9494,
+		"QLD": 123123,
+		"NT":  73694,
+		"WA":  8382,
+		"SA":  3737,
+		"VIC": 93832,
+		"TAS": 88430,
+		"ACT": 83729,
+	}
+	for k, v := range statePopulations {
+		fmt.Println(k, v)
+	}
+	fmt.Println()
+
+	// loop through a string
+	f := "Hello Go!"
+	for k, v := range f {
+		fmt.Println(k, v) // unicode representation of each character
+	}
+	fmt.Println()
+	for k, v := range f {
+		fmt.Println(k, string(v)) // string representation of the unicode of each character
 	}
 }
