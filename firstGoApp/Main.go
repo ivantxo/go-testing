7119a9c28047cb92d2fa54f6007bac1a6f05d469
ivantxo <ivan.ramirezf@gmail.com>
2020-12-08 05:59:46 +1000

    Control flow, panic, panic and recover

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
-	// this will print start, this was deferred, panic: something bad happened
-	// the order of execution is panic after defer
+	// panic and recover from panic
 	fmt.Println("start")
-	defer fmt.Println("this was deferred")
-	panic("something bad happened")
+	panicker()
 	fmt.Println("end")
+}
+
+func panicker() {
+	fmt.Println("about to panic")
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error: ", err)
+		}
+	}()
+	panic("something went bad")
+	fmt.Println("done panicking")
 }
