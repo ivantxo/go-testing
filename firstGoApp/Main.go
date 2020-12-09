19ea77dba7c9d86d38dee720d65b65e6e9338b5d
ivantxo <ivan.ramirezf@gmail.com>
2020-12-10 06:33:01 +1000

    Pointers 3

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -32,6 +32,30 @@ func main() {
 	ms = new(myStruct)
 	ms.foo = 42         // (*ms).foo this will also work
 	fmt.Println(ms.foo) // (*ms).foo this will also work
+	fmt.Println("")
+
+	// arrays don't work by reference
+	aarr := [3]int{1, 2, 3}
+	barr := aarr
+	fmt.Println(aarr, barr)
+	aarr[1] = 42 // if I change [1] on variable aarr, it doesn't change on barr
+	fmt.Println(aarr, barr)
+	fmt.Println("")
+
+	// slices work by reference
+	aarr1 := []int{1, 2, 3}
+	barr1 := aarr1
+	fmt.Println(aarr1, barr1)
+	aarr1[1] = 42 // if I change [1] on variable aarr1, it will change on barr1
+	fmt.Println(aarr1, barr1)
+	fmt.Println("")
+
+	// maps also work by reference
+	amap := map[string]string{"foo": "bar", "baz": "buzz"}
+	bmap := amap
+	fmt.Println(amap, bmap)
+	amap["foo"] = "qux" // if I change index "foo" of amap, it will change index "foo" on bmap
+	fmt.Println(amap, bmap)
 }
 
 type myStruct struct {
