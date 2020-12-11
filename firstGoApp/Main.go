90be1e9f5d5202d6638ac9a3e96890ff0e955ff0
ivantxo <ivan.ramirezf@gmail.com>
2020-12-12 06:18:03 +1000

    Functions 4, variadic parameters 2

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -5,15 +5,15 @@ import (
 )
 
 func main() {
-	sum(1, 2, 3, 4, 5)
+	sum("The sum is: ", 1, 2, 3, 4, 5)
 }
 
 // function with variadic parameters
-func sum(values ...int) {
+func sum(msg string, values ...int) {
 	fmt.Println(values)
 	result := 0
 	for _, v := range values {
 		result += v
 	}
-	fmt.Println("The sum is: ", result)
+	fmt.Println(msg, result)
 }
