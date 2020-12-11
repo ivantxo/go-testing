a98163dda1bd501ad65363b73903d25f76bdd604
ivantxo <ivan.ramirezf@gmail.com>
2020-12-12 06:34:45 +1000

    Functions 7, named return values

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -6,14 +6,13 @@ import (
 
 func main() {
 	s := sum(1, 2, 3, 4, 5)
-	fmt.Println("The sum is: ", *s)
+	fmt.Println("The sum is: ", s)
 }
 
 // function with variadic parameters
-func sum(values ...int) *int {
-	result := 0
+func sum(values ...int) (result int) {
 	for _, v := range values {
 		result += v
 	}
-	return &result
+	return
 }
