0491ce784618e25d08564803abc3d9300980a3bf
ivantxo <ivan.ramirezf@gmail.com>
2020-12-14 05:41:11 +1000

    Functions 9, Anonymous functions

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -5,16 +5,19 @@ import (
 )
 
 func main() {
-	r, err := divide(5.0, 0.0)
+	var divide func(float64, float64) (float64, error)
+
+	divide = func(a, b float64) (float64, error) {
+		if b == 0.0 {
+			return 0.0, fmt.Errorf("Cannot divide by zero")
+		}
+		return a / b, nil
+
+	}
+	d, err := divide(5.0, 0.0)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println(r)
-}
-
-func divide(a, b float64) (float64, error) {
-	if b == 0.0 {
-		return 0.0, fmt.Errorf("Cannot divide by zero")
-	}
-	return a / b, nil
+	fmt.Println(d)
 }
