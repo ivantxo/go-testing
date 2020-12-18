facf9d312f5c9ed19c0f6a99402644f9b03bc14f
ivantxo <ivan.ramirezf@gmail.com>
2020-12-18 15:17:32 +1000

    Goroutines 4, Setting the number of threads with GOMAXPROCS

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -2,39 +2,9 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"runtime"
 )
 
-// Asynchronous routine using Wait Group
-var wg = sync.WaitGroup{}
-var counter = 0
-var m = sync.RWMutex{}
-
 func main() {
-	// This will print Hello <counter>, but the counter will be inconsistent 1, 2, 1, 1, 10, 9, 9
-	for i := 0; i < 10; i++ {
-		wg.Add(2)
-		m.RLock()
-		go sayHello()
-		m.Lock()
-		go increment()
-	}
-	wg.Wait()
-}
-
-// func sayHello() {
-// 	fmt.Println("Hello #", counter)
-// 	wg.Done()
-// }
-
-func sayHello() {
-	fmt.Println("Hello #", counter)
-	m.RUnlock()
-	wg.Done()
-}
-
-func increment() {
-	counter++
-	m.Unlock()
-	wg.Done()
+	fmt.Println("Threads\n", runtime.GOMAXPROCS(-1))
 }
