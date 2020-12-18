e6f8f2ccb129f316b326104300e6f892cd38d0d9
ivantxo <ivan.ramirezf@gmail.com>
2020-12-18 15:38:08 +1000

    Channels 1, sending and receiving data to/from channel

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"sync"
 )
 
+var wg = sync.WaitGroup{}
+
 func main() {
-	fmt.Println("Threads\n", runtime.GOMAXPROCS(-1))
+	ch := make(chan int)
+	wg.Add(2)
+	// receiving go routine
+	go func() {
+		i := <-ch // pulling data from the channel
+		fmt.Println(i)
+		wg.Done()
+	}()
+
+	// sending go routine
+	go func() {
+		ch <- 42 // sending data to the channel
+		wg.Done()
+	}()
+	wg.Wait()
 }
