72548098953003f0ae3fc278a0e0eef37543d90b
ivantxo <ivan.ramirezf@gmail.com>
2020-12-18 14:28:45 +1000

    Goroutines 3, Mutex and concurrent programming

--- firstGoApp/Main.go
+++ firstGoApp/Main.go
@@ -7,14 +7,34 @@ import (
 
 // asynchronous routine using Wait Group
 var wg = sync.WaitGroup{}
+var counter = 0
+var m = sync.RWMutex{}
 
 func main() {
-	var msg = "Hello"
-	wg.Add(1)
-	go func(msg string) {
-		fmt.Println(msg)
-		wg.Done()
-	}(msg)
-	msg = "Goodbye"
+	// This will print Hello <counter>, but the counter will be inconsistent 1, 2, 1, 1, 10, 9, 9
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock()
+		go sayHello()
+		m.Lock()
+		go increment()
+	}
 	wg.Wait()
+}
+
+// func sayHello() {
+// 	fmt.Println("Hello #", counter)
+// 	wg.Done()
+// }
+
+func sayHello() {
+	fmt.Println("Hello #", counter)
+	m.RUnlock()
+	wg.Done()
+}
+
+func increment() {
+	counter++
+	m.Unlock()
+	wg.Done()
 }
